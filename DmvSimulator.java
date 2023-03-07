10c1a52fd2700762a14a633772113e14f6c23163
atoomer <atoomer@quinnipiac.com>
2023-03-07 12:27:19 -0500

    Welcome message. Gives wait number between 1 and 100. Calls every number and circles back around starting from the number after wait number. Sorry message.

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,5 +1,16 @@
+import java.lang.Math;
+
 public class DmvSimulator{
     public static void main(String[]args){
-        
+        int waitNum = (int)(Math.random() * (100)+1);
+        System.out.println("welcome to the DMV.");
+        System.out.println("You are number " + waitNum + ". Please wait to be called.");
+        for(int i = waitNum; i < 100; i++){
+            System.out.print((i+1)+" ");
+        }
+        for(int i = 1; i <= waitNum; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println("\nSorry, you do not have the correct paperwork.");
     }
 }

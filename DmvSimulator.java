46a05a10563e1c5ada31c4fd9e45150fe4bf49a3
atoomer <atoomer@quinnipiac.com>
2023-03-07 16:52:48 -0500

    added miracle

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -3,6 +3,7 @@ import java.lang.Math;
 public class DmvSimulator{
     public static void main(String[]args){
         int waitNum = (int)(Math.random() * (200)+1);
+        int chance = (int)(Math.random() * (100)+1);
         System.out.println("welcome to the DMV.");
         System.out.println("You are number " + waitNum + ". Please wait to be called.");
         for(int i = waitNum; i < 200; i++){
@@ -11,6 +12,11 @@ public class DmvSimulator{
         for(int i = 1; i <= waitNum; i++){
             System.out.print(i + " ");
         }
-        System.out.println("\nGet the right paperwork this instant or else!");
+        if (chance == 1){
+            System.out.println("\nYou have all the required paperwork. You are all set!");
+        }
+        else{
+            System.out.println("\nGet the right paperwork this instant or else!");
+        }
     }
 }
